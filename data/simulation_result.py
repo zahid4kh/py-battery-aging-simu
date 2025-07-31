34ebebb7ebcefbe1c3084696deedbc63dddd12fd
zahid4kh <halilzahid@gmail.com>
2025-07-31 23:56:57 +0200

    fixed packaging issue & naming

--- data/simulation_result.py
+++ data/simulation_result.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from typing import List
-from bus import Bus
-from battery_state import BatteryState
-from operating_condition import OperatingCondition
+from .bus import Bus
+from .battery_state import BatteryState
+from .operating_condition import OperatingCondition
 
 @dataclass
 class SimulationResult:
