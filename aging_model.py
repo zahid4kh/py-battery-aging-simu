4ea9ee786be22fc71fcf07ef065b5c3b2fa3b21b
zahid4kh <halilzahid@gmail.com>
2025-08-01 08:49:17 +0200

    introduced chemical term to cyclic aging model

--- aging_model.py
+++ aging_model.py
@@ -16,19 +16,24 @@ class AgingModel:
 
         return self.params.pre_exp_factor_calendar * temp_term * soc_term * time_term
 
-    def calculate_cyclic_aging(self, efc: float, temp_celsius: float, avg_soc: float, dod: float) -> float:
+    def calculate_cyclic_aging(self, efc: float, temp_celsius: float, avg_soc: float, avg_dod: float) -> float:
+        if efc <= 0:
+            return 0.0
+
         temp_kelvin = celsius_to_kelvin(temp_celsius)
         temp_term = np.exp(-self.params.activation_energy_cyclic / (self.params.gas_constant * temp_kelvin))
-        stress_term = self._calculate_stress_amplitude(avg_soc, dod)
+        stress_term = self._calculate_stress_amplitude(avg_soc, avg_dod)
         efc_term = efc ** self.params.efc_exponent
+        soc_chemical_term = 3.90e-3 * avg_soc + 0.20 # chemical stress -> linear relationship Equation 4.11
 
-        return self.params.cyclic_factor * stress_term * temp_term * efc_term
+        return self.params.cyclic_factor * stress_term * temp_term * efc_term * soc_chemical_term
 
-    def _calculate_stress_amplitude(self, avg_soc: float, dod: float) -> float:
-        soc_min = avg_soc - dod / 2.0
-        soc_max = avg_soc + dod / 2.0
+    def _calculate_stress_amplitude(self, avg_soc: float, avg_dod: float) -> float:
+        soc_min = max(0.0, avg_soc - avg_dod / 2.0)
+        soc_max = min(1.0, avg_soc + avg_dod / 2.0)
         return self._calculate_polynomial(soc_max) - self._calculate_polynomial(soc_min)
 
+    # Graphite expansion function; Equation 4.6 && Table 4.2
     def _calculate_polynomial(self, soc: float) -> float:
         coeffs = [2.74e-13, -8.39e-11, 8.38e-9, -2.39e-7, -5.05e-6, 9.70e-5, 0.02, -6.19e-3]
         result = 0.0
