e0640b4378b55b675455cbbef715d08bb2f68b57
zahid4kh <halilzahid@gmail.com>
2025-08-18 10:20:33 +0200

    corrected params

--- data/aging_parameters.py
+++ data/aging_parameters.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class AgingParameters:
     activation_energy_calendar: float = 36360.0
-    activation_energy_cyclic: float = 25000.0 # randomly trying
+    activation_energy_cyclic: float = 36360.0
     gas_constant: float = 8.314
     time_exponent: float = 0.789
     efc_exponent: float = 0.98
-    pre_exp_factor_calendar: float = 5.0e-3
+    pre_exp_factor_calendar: float = 2.15e+4
     cyclic_factor: float = 2.5
