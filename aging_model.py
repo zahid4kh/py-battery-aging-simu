268014282b64abb2fe869d3e171ade7cdcda8e2e
zahid4kh <halilzahid@gmail.com>
2025-08-21 09:02:47 +0200

    added functions to calculate the SOC dependency loss and selecting the linear one; added simplified active material loss function; c2, c3, c4, c5 are given in %
    Signed-off-by: zahid4kh <halilzahid@gmail.com>

--- aging_model.py
+++ aging_model.py
@@ -19,32 +19,57 @@ class AgingModel:
         soc_term = (self.params.gamma_calendar * avg_soc +
                     self.params.sigma_calendar)
 
-        return self.params.pre_exp_factor_calendar * temp_term * soc_term * time_term
+        calendar_loss = self.params.pre_exp_factor_calendar * temp_term * soc_term * time_term
+
+        return calendar_loss
 
     def calculate_cyclic_aging(self, efc: float, temp_celsius: float, avg_soc: float, avg_dod: float) -> float:
         if efc <= 0:
             return 0.0
 
+        sei_loss = self._calculate_sei_loss(efc, temp_celsius, avg_soc, avg_dod)
+        am_loss = self._calculate_active_material_loss(efc, avg_soc, avg_dod)
+        return sei_loss + am_loss
+
+    def _calculate_sei_loss(self, efc: float, temp_celsius: float, avg_soc: float, avg_dod: float) -> float:
         temp_kelvin = celsius_to_kelvin(temp_celsius)
         temp_term = np.exp(-self.params.activation_energy_cyclic /
                            (self.params.gas_constant * temp_kelvin))
 
-        sei_loss = self._calculate_sei_loss(efc, temp_kelvin, avg_soc, avg_dod)
-
-        return sei_loss  # for now, returning only SEI loss without Active Material loss
-
-    def _calculate_sei_loss(self, efc: float, temp_kelvin: float, avg_soc: float, avg_dod: float) -> float:
-        temp_term = np.exp(-self.params.activation_energy_cyclic /
-                           (self.params.gas_constant * temp_kelvin))
-
-        stress_amplitude = self._calculate_stress_amplitude(
-            avg_soc, avg_dod)
-
-        soc_chemical_term = self.params.c3 * avg_soc + self.params.c4
-
+        stress_amplitude = self._calculate_stress_amplitude(avg_soc, avg_dod)
+        soc_chemical_term = self._calculate_soc_dependency_L(avg_soc)
         efc_term = efc ** self.params.efc_exponent
 
-        return self.params.c2 * stress_amplitude * temp_term * efc_term * soc_chemical_term  # Equation 4.8
+        sei_loss_percent = self.params.c2 * stress_amplitude * temp_term * efc_term * soc_chemical_term
+
+        return sei_loss_percent / 100.0
+
+    def _calculate_soc_dependency_E(self, avg_soc: float) -> float:
+        """Exponential SoC dependency (Equation 4.9)"""
+        avg_soc_percent = avg_soc * 100
+        return self.params.c3 * np.exp(self.params.c4 * avg_soc_percent)
+
+    def _calculate_soc_dependency_S(self, avg_soc: float) -> float:
+        """Sigmoidal SoC dependency (Equation 4.10)"""
+        avg_soc_percent = avg_soc * 100
+        return 1.0 / (1.0 + np.exp(-self.params.c3 * (avg_soc_percent - self.params.c4)))
+
+    def _calculate_soc_dependency_L(self, avg_soc: float) -> float:
+        """Linear SoC dependency (Equation 4.11)"""
+        avg_soc_percent = avg_soc * 100
+        return self.params.c3 * avg_soc_percent + self.params.c4
+
+    def _calculate_active_material_loss(self, efc: float, avg_soc: float, avg_dod: float) -> float:
+        """for DoD > 60%"""
+        if avg_dod <= 0.6:
+            return 0.0
+
+        stress_amplitude = self._calculate_stress_amplitude(avg_soc, avg_dod)
+
+        # Paris Law analogy
+        am_loss = self.params.c5 * (stress_amplitude ** self.params.m) * efc
+
+        return am_loss / 100.0
 
     def _calculate_stress_amplitude(self, avg_soc: float, avg_dod: float) -> float:
         soc_min = max(0.0, avg_soc - avg_dod / 2.0)
